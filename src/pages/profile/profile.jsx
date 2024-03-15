fe8942be8bc4627496c2e6078bebe5f8163a8c14
spectre9710 <ramialan176@gmail.com>
2024-03-15 00:05:23 -0600

    componente profile y css

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,9 +1,30 @@
+import "./profile.css"
+import font_page from "../../components/img/Portada3.webp"
+import img_perfil from "../../components/img/Perfil2.jpeg"
+
 export function Profile() {
     return (
-        <>
-            <div className="">
-                <h5>Pagina de perfil</h5>
+
+        <div className="profile">
+            <div className="left-pane">
+                <div className="header">
+                    <img className="front-page" src={font_page} alt="" />
+                    <div className="section-left">
+                    <img className="img-profile" src={img_perfil} alt="" />
+                    <div className="name">
+                        <h1>Joanna Rosenbaum</h1>
+                        <h2>Ing. Sistemas de la información</h2>
+                        <h2>Chiapas, México</h2>
+                    </div>
+                    </div>
+                    <div className="section-right">
+                    <div className="hobbies">
+                        <h1>Intereses</h1>
+                    </div>
+                </div>
+                </div>
             </div>
-        </>
+            <div className="right-pane" />
+        </div>
     );
 }
